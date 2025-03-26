6ee27109c6c24039f12a9ec913eb396dcf0fa00c
Meghana <meghanamanchal@gmail.com>
2025-03-26 13:52:55 +0530

    Add new project images and update image paths in constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ const projects = [
       title: "Hospital Management System",
       description: "The Hospital Management System helps patients book doctor appointments faster, saving time and improving their experience..",
       technologies: ["Node.js", "MongoDB","OAuth"],
-      image: '../src/public/hosp.jpg',
+      image: 'public/hosp.jpg',
       link: "https://capstone-mediconnect.vercel.app/",
       author: "Meghana"
     },
@@ -11,7 +11,7 @@ const projects = [
       title: "Quiz App",
       description: "A Quiz App for easily taking and tracking quizzes.",
       technologies: ["React", "Tailwind CSS"],
-      image: "../src/public/quiz.jpg",
+      image: "public/quiz.jpg",
       link: "https://quiz-app-general.vercel.app/",
       author: "Meghana"
     },
@@ -19,7 +19,7 @@ const projects = [
       title: "Calculator App",
       description: "A Calculator App for performing quick, accurate, and easy calculations.",
       technologies: ["Next.js", "Node.js"],
-      image: "../src/public/calculator.png",
+      image: "public/calculator.png",
       link: "https://calculator-app-rouge-six.vercel.app/",
       author: "Meghana"
     },
@@ -27,7 +27,7 @@ const projects = [
       title: "Personal Portfolio Website",
       description: "A personal website showcasing projects, blogs, and contact information.",
       technologies: ["Gatsby", "GraphQL", "Styled Components"],
-      image: "../src/public/image.png",
+      image: "public/image.png",
       link: "https://portfolio.example.com",
       author: "Meghana"
     }
